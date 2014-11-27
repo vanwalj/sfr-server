67015587649d45b1ecbf78f1445f480c380ada98
Jordan <j.vanwall@gmail.com>
2014-11-27 21:09:57 +0000

    Added Winston Slack integration.

--- config/winston.js
+++ config/winston.js
@@ -2,6 +2,16 @@
  * Created by Jordan on 25/11/14.
  */
 
-//var winston = require('winston');
+var winston = require('winston'),
+    Slack   = require('winston-slack-pls').Slack;
+
+winston.setLevels(winston.config.npm.levels);
+
+if (process.npde.env == "production") {
+    winston.add(Slack, {
+        apiToken: "xoxp-3090809371-3090809375-3102462589-741246",
+        channel: "C0331UB2C"
+    });
+}
 
 //Add more logger
