71181957f478195435b61261a29f03892c371910
Jordan Van Walleghem <j.vanwall@gmail.com>
2014-12-25 10:42:11 +0100

    Winston handle exception !

--- config/winston.js
+++ config/winston.js
@@ -10,7 +10,8 @@ winston.setLevels(winston.config.npm.levels);
 if (process.env.NODE_ENV == "production") {
     winston.add(Slack, {
         apiToken: "xoxp-3090809371-3090809375-3102462589-741246",
-        channel: "C0331UB2C"
+        channel: "C0331UB2C",
+        handleExceptions: true
     });
 }
 
